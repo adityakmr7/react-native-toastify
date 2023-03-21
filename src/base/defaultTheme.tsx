c68d05ad1f34dcbfde848c4008a90f9346ac31c5
dev-adityakmr <dev.adityakmr@gmail.com>
2023-03-21 10:51:22 +0530

    feat: added support for theme

---
+++ src/base/defaultTheme.tsx
@@ -0,0 +1,20 @@
+import type { ThemeProps } from './interface';
+
+export const defaultTheme: ThemeProps = {
+  error: {
+    primary: '#d32f2f',
+    secondary: '#fff',
+  },
+  success: {
+    primary: '#2e7d32',
+    secondary: '#fff',
+  },
+  warning: {
+    primary: '#ff9800',
+    secondary: '#fff',
+  },
+  default: {
+    primary: '#1976d2',
+    secondary: '#fff',
+  },
+};
--- src/base/interface.ts
+++ src/base/interface.ts
@@ -1,9 +1,22 @@
+export type ToastType = 'default' | 'success' | 'error' | 'warning';
 export interface showToastProps {
   message?: string;
   duration?: number;
+  type?: ToastType;
 }
 export interface ToastContextProps {
   Toast: {
     showToast: ({ message, duration }: showToastProps) => void;
   };
+}
+
+export interface ThemeType {
+  primary: string;
+  secondary: string;
+}
+export interface ThemeProps {
+  default: ThemeType;
+  error: ThemeType;
+  success: ThemeType;
+  warning: ThemeType;
 }
