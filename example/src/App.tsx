a89ae53009cecc7f103e87f07c5a914ad840cfc6
dev-adityakmr <dev.adityakmr@gmail.com>
2023-03-22 15:57:45 +0530

    chore: change color

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,7 +5,7 @@ import { ToastProvider } from 'react-native-toastify';
 import ToastComponent from './ToastComponent';
 export const theme: any = {
   error: {
-    primary: '#00000',
+    primary: '#000000',
     secondary: '#fff',
   },
   success: {
